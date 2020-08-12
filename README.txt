73a0e40592b1fcd7d615012563ba43069b5dd886
gitUserNameABC <gituserABC@zzz.com>
2020-08-12 13:35:29 -0400

    aa

---
+++ README.txt
@@ -0,0 +1,7 @@
+
+hot reload when debugger running:
+This option controls Run | Debugging Actions | Reload Changed Classes action behavior.
+If it's turned on, the make process is performed before searching and reloading changed classes.
+... tried it but seems to be a one time command
+.... added it to icons... file-settings-debug toolbar top window-add
+... then add it from main toolbar-run-debugging actions
---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class HelloController {
+	public static int currVal = 0;
+
+	@RequestMapping("/")
+	public String index() {
+		return new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()) + " Greetings from Spring Boot!...";
+	}
+
+	@GetMapping("/echoIn/{variableID}/{echoValue}")
+	public String echoIn(@PathVariable String variableID, @PathVariable String echoValue) {
+		int variableID2 = Integer.parseInt(variableID);
+		currVal = Integer.parseInt(echoValue);
+		return "/echo/input value: " +  echoValue + " at ..... " + new SimpleDateFormat("mm 'min', ss 'sec'").format(new Date());
+	}
+
+	@GetMapping("/echoOut/{variableID}")
+	public String echoOut(@PathVariable String variableID) {
+		int variableID2 = Integer.parseInt(variableID);
+		return "/echoOut/output/ variableID: " +  variableID2 + " value: <b>" + currVal + "</b> at ..... " + new SimpleDateFormat("mm 'min', ss 'sec'").format(new Date());
+	}
+
+}
